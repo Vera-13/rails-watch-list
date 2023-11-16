91d0df302cd75e7c7ca42f99544ceec3b510159a
Vera Ciocan <guess13grl@gmail.com>
2023-11-16 16:35:14 +0100

    Watch list day 1

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: 'lists#index'
+  get 'lists/new', to: 'lists#new'
+  post 'lists', to: 'lists#create'
+  get 'lists/:id', to: 'lists#show', as: :list
+  get 'lists/:id/bookmarks/new', to: 'bookmarks#new', as: :new_bookmark
+  post 'lists/:id/bookmarks', to: 'bookmarks#create', as: :list_bookmarks
+  delete 'lists/:id/bookmarks/:id', to: 'bookmarks#destroy', as: :bookmark
 end
